475b5fc6f932ae9c0065417dd2bf60fe8009ecd1
Octo_Naila.stu <nailatabassum883@gmail.com>
2026-08-07 13:28:55 +0600

    Create 1startriangle.py

---
+++ 1startriangle.py
@@ -0,0 +1,11 @@
+N=int(input('Enter number of rows:'))
+for i in range(N):
+  for j in range(N-i-1):
+    print(' ',end='')
+  for j in range(2*i+1):
+    print('*',end='')
+  print()
+
+
+
+#Okay!! so it's my python practice not a professional one of course.
